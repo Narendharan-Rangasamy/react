7cbf8507d06e9004fcfe6b4ba7e31c04455dc131
Narendharan-Rangasamy <narendharan2310@gmail.com>
2022-07-30 09:40:42 +0530

    App.js code correction update

--- src/App.js
+++ src/App.js
@@ -1,47 +1,30 @@
-import React, { Component } from "react";
+import React from 'react';
 
-class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            // myarr: ["Sam","Chris","Joe"]
-            myarr: [{ studentID: 1001, studentName: "Naren", degree: "B.Sc IT", location: "Cbe", mobileNo: 12345 }]
-        };
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      myarr: [{ studentID: 1001, studentName: "Naren", degree: "B.Sc IT", location: "Cbe", mobileNo: 12345 }]
     }
-
-    render() {
-        return (
-            <React.Fragment>
-                <p>My ID is -</p>  {
-                    this.state.myarr.map(myarr =>
-                    (<b key={myarr.studentID}>
-                        {myarr.studentID}
-                    </b>)
-                    )}
-
-                <p>My Name is -</p>  {
-                    this.state.myarr.map(myarr =>
-                    (<b key={myarr.studentName}>
-                        {myarr.studentName}
-                    </b>)
-                    )}
-
-                <p>My Degree is -</p>  {
-                    this.state.myarr.map(myarr =>
-                    (<b key={myarr.degree}>
-                        {myarr.degree}
-                    </b>)
-                    )}
-
-                <p>My Location is -</p>  {
-                    this.state.myarr.map(myarr =>
-                    (<b key={myarr.location}>
-                        {myarr.location}
-                    </b>)
-                    )}
-            </React.Fragment>
-        );
-    }
+  }
+  render(){
+    const { myarr } = this.state
+    return(
+      <>
+        {myarr.map((d,i) => {
+          return(
+            <div key={i}>
+              <h3>{d.studentID}</h3>
+              <h3>{d.studentName}</h3>
+              <h3>{d.degree}</h3>
+              <h3>{d.location}</h3>
+              <h3>{d.mobileNo}</h3>
+            </div>
+          )
+        })}
+      </>
+    )
+  }
 }
 
-export default App;
+export default App
