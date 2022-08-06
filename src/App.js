c2cb2d005c7332f38a8b0c4bcdf9b0b3834fee51
Narendharan-Rangasamy <narendharan2310@gmail.com>
2022-08-06 07:02:04 +0530

    fallback code commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,62 @@
-import React from 'react';
+import React from 'react'
+import studentData from './studentData.json'
 
-class App extends React.Component {
+class App extends React.Component{
   constructor(){
-    super()
+    super();
     this.state = {
-      myarr: [{ studentID: 1001, studentName: "Naren", degree: "B.Sc IT", location: "Cbe", mobileNo: 12345 }]
+      heading  : "STUDENT DATA",
+      studentDetails : [],
+      dataEmpty :false
     }
   }
+
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({
+        dataEmpty :  false
+      })
+    }, 0);
+
+    setTimeout(() => {
+      this.setState({
+        heading : "STUDENT DATA INFO",
+        studentDetails : studentData,
+        dataEmpty :true
+      })
+    }, 4000);
+  }
+
+  shouldComponentUpdate(){
+    return true;
+  }
+
+
   render(){
-    const { myarr } = this.state
+    const {studentDetails, heading, dataEmpty} = this.state
     return(
       <>
-        {myarr.map((d,i) => {
-          return(
-            <div key={i}>
-              <h3>{d.studentID}</h3>
-              <h3>{d.studentName}</h3>
-              <h3>{d.degree}</h3>
-              <h3>{d.location}</h3>
-              <h3>{d.mobileNo}</h3>
-            </div>
-          )
-        })}
+      <h1><u>{heading}</u></h1>
+      {/* fallback method - It will check the lenght of aray declared above and return below template
+        ifthe value is 0. In simple words it will display below text
+        during the settime out period
+        
+        In below case it will display loading until 4 secs as setimeout declared for 4 secs*/}
+      {!studentDetails.length && !dataEmpty && <h2>loading...</h2>}
+      {/* The below condition is used to display no result if data not found
+          We have decalred dataEmpty boolean value in class and set it as false using componentDidMount.
+          If the array is found to be empty at componentDidMount then we are setting dataEmpty as true(line No-25)
+          and it will run below condition */}
+      {dataEmpty && !studentDetails.length && <h2>Ops!! No data found</h2>}
+      {studentDetails.map((data, index) => {
+        return(
+          <div key={index}>
+            <h2>{data.studentID}</h2>
+            <h2>{data.studentName}</h2>
+            <h2>{data.studentDegree}</h2>
+          </div>
+        )
+      })}
       </>
     )
   }
